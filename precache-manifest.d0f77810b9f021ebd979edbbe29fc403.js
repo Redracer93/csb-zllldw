577b4b4ea0b6338383af6279b8d945db025237fb
Code Sandbox <hello@codesandbox.io>
2022-07-14 18:59:41 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d0f77810b9f021ebd979edbbe29fc403.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b778d3159a38f80aa334172029b67cc",
+    "url": "https://redracer93.github.io/csb-zllldw/index.html"
+  },
+  {
+    "revision": "a2eba9871ba53c1b9da8",
+    "url": "https://redracer93.github.io/csb-zllldw/static/css/main.f59dff40.chunk.css"
+  },
+  {
+    "revision": "59663d545433ef88b6d6",
+    "url": "https://redracer93.github.io/csb-zllldw/static/js/2.5c238370.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "https://redracer93.github.io/csb-zllldw/static/js/2.5c238370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2eba9871ba53c1b9da8",
+    "url": "https://redracer93.github.io/csb-zllldw/static/js/main.729fcf6c.chunk.js"
+  },
+  {
+    "revision": "c84a49f11308a107a62f",
+    "url": "https://redracer93.github.io/csb-zllldw/static/js/runtime-main.89bdc0f8.js"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "https://redracer93.github.io/csb-zllldw/static/media/logo.be622899.svg"
+  }
+]);
